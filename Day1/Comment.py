9858027efb0dc1972abdf493d0e01163f1697877
Mehta Deven <devenmehta2@gmail.com>
2024-07-01 11:27:28 +0530

    Add files via upload

---
+++ Day1/Comment.py
@@ -0,0 +1,16 @@
+#This is comments
+#print hello world
+
+print("Hello World")
+
+#This is single line comment
+
+'''This  is multiline
+    comment   of 
+    multiline comments
+'''
+
+"""This is
+    multiline
+"""
+
